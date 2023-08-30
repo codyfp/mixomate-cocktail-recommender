c8314a8569b123fa374cf77dfd7a81ff3b9ea42f
pharapeti <patrice.harapeti@gmail.com>
2023-08-30 16:25:54 +1000

    Create stub for recommentations model in recommentations service

---
+++ apps/mixio-mate-recommenders/app/recommendations/model.py
@@ -0,0 +1,5 @@
+# Create Tensorflow model object
+# Load model.json generated from modelGenerator into Tensflow model object
+
+# The implementation of this file will be similar to the Tensorflow.js implementation
+# found at: https://github.com/risenW/Tensorflowjs_Projects/blob/master/recommender-sys/recommender-books/model.js
