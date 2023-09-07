1b8bc3a5c56205f96ea70738bbefdd383117aa43
aryangupta1 <aryangupta1@outlook.com>
2023-09-07 23:22:47 +1000

    new logo and custom color

--- apps/mixo-mate-frontend/components/Header.tsx
+++ apps/mixo-mate-frontend/components/Header.tsx
@@ -1,12 +1,11 @@
 const Header = ({ logoSrc }) => {
     return (
-        <header className="bg-orange-600 p-4 shadow-md">
-          <div className="container mx-auto flex items-center">
-            <img src={logoSrc} alt="Logo" className="h-10 w-auto opacity-75" />
+        <header className="bg-logo-orange p-4 shadow-md">
+          <div className="container mx-auto flex items-center justify-between">
+            <img src={logoSrc} alt="Logo" className="h-10 w-auto ml-0" />
           </div>
         </header>
-
-      )
+    )
 }
 
 export default Header
