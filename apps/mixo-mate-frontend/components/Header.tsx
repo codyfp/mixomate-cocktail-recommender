8feefaa5b2031d3a41d3a112c6c1235f10fa3036
Aryan Gupta <aryan.gupta1@outlook.com>
2023-09-10 21:17:13 +1000

    feature(flavour-profile) (#12)
    * remove heading text
    
    * remove text and add space
    
    * Center and spacing
    
    * Create basic flavourProfile card with flavour chips
    
    * make it a bit bigger and more spacing
    
    * Actually don't need control buttons for this component
    
    * Create close icon
    
    * 3 more chips
    
    * cleanup
    
    * Use the font awesome package instead of relying on cdnjs link
    
    * Put the space back
    
    ---------
    
    Co-authored-by: aryangupta1 <aryangupta1@outlook.com>

--- apps/mixo-mate-frontend/components/Header.tsx
+++ apps/mixo-mate-frontend/components/Header.tsx
@@ -5,12 +5,6 @@ const Header = ({ logoSrc }) => {
             
             <img src={logoSrc} alt="Logo" className="h-full w-auto blend-multiply" />
 
-            <div className="space-x-4">
-                <a href="#" className="text-white hover:text-gray-200 font-bold text-xl">About</a>
-                <a href="#" className="text-white hover:text-gray-200 font-bold text-xl">Feedback</a>
-                <a href="#" className="text-white hover:text-gray-200 font-bold text-xl">Support</a>
-            </div>
-
             <div className="cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-white">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
