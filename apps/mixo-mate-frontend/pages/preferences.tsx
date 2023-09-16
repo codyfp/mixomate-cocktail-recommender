db6a0065f2c9345c8eb6f876e7bec5702ddacc48
jdroman1912 <110380285+jdroman1912@users.noreply.github.com>
2023-09-16 22:48:04 +1000

    Likes-Dislikes Component (#9)
    * Test
    
    * Test
    
    * Refactor preferences
    
    - Add RenderCurrentStep to render steps dynamically
    - Add component StepControlButton
    
    Changed the switch into an array (at the end it does the same, but I think it can help when including more components).
    
    * Styling changes
    
    * Cocktail image and changes from before
    
    There were some changes that were not included in the previous commit after solving the conflicts
    
    * Likes and Dislikes component
    
    * Added some styling
    
    Some styling added into the CSS file
    
    * Added the placeholder for the chips

--- apps/mixo-mate-frontend/pages/preferences.tsx
+++ apps/mixo-mate-frontend/pages/preferences.tsx
@@ -6,65 +6,109 @@ import FlavourProfile from "@/components/FlavourProfile";
 
 export default function Preferences() {
   const [step, setStep] = useState(0);
+ 
 
-  const ControlButtons: React.FunctionComponent<{ isLastStep?: Boolean }> = ({ isLastStep = false }) => {
-    return (
-      <div className='flex flex-row gap-4'>
-        {step != 0 && <button className='p-2 bg-custom-orange rounded-3xl text-blue-50 w-[120px]' onClick={() => setStep(step - 1)}>Back</button>}
-        {!isLastStep && <button className='p-2 bg-custom-orange rounded-3xl text-blue-50 w-[120px]' onClick={() => setStep(step + 1)}>Next</button>}
-      </div>
-    )
-  }
 
-  const RenderCurrentStep = () => {
-    switch (step) {
-      case 0:
-        return (
-          <div className='flex flex-col'>
-            <p>What do you like?</p>
-            <LikesAndDislikes />
-            <ControlButtons />
-          </div>
-        )
-      case 1:
-        return (
-        <div className='flex flex-col justify-center items-center'>
+const steps = [
+  LikesAndDislikes,
+  () => (
+    <div className='flex flex-col justify-center items-center'>
           <br></br>
           <FlavourProfile onSubmit={() => setStep(step + 1)} onClose={() => setStep(step - 1)} />
-        </div>
-        )
-      case 2:
-        return (
-          <div className='flex flex-col'>
-            <p>Any allergens?</p>
-            <p>Coming soon...</p>
-            <ControlButtons />
-          </div>
-        )
-      default:
-        return (
-          <div className='flex flex-col'>
-            <p>Ready to get recommendations?</p>
-            <Link href='/recommendations'>
-              <button className='p-2 bg-blue-100 rounded-3xl text-blue-500 w-[120px]'>{"Let's go!"}</button>
-            </Link>
-            <ControlButtons isLastStep={true} />
-          </div>
-        )
-    }
-  }
+    </div>
+  ),
+  () => (
+    <>
+      <p>Any allergens?</p>
+      <p>Coming soon...</p>
+    </>
+  ),
+  () => (
+    <>
+      <p>Ready to get recommendations?</p>
+
+      <Link href="/recommendations">
+        <button className="p-2 bg-blue-100 rounded-3xl text-blue-500 w-[120px]">
+          {"Let's go!"}
+        </button>
+      </Link>
+    </>
+  ),
+];
+
+function RenderCurrentStep() {
+  const [step, setStep] = useState(0);
+  const CurrentStep = steps[step];
 
   return (
-    <div>
+    <div className="p-8">
+      <div className="mb-8">
+        <CurrentStep />
+      </div>
+
+      <div className="flex justify-end">
+        <div className="flex flex-row gap-4">
+          {step !== 0 && (
+            <StepControlButton onClick={() => setStep(step - 1)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="white"
+                  d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z"
+                />
+              </svg>
+            </StepControlButton>
+          )}
+
+          {step < steps.length - 1 && (
+            <StepControlButton onClick={() => setStep(step + 1)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="white"
+                  d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"
+                />
+              </svg>
+            </StepControlButton>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function StepControlButton({
+  onClick,
+  children,
+}: {
+  onClick: () => void;
+  children: React.ReactNode;
+}) {
+  return (
+    <button
+      className="flex items-center justify-center p-2 bg-custom-orange rounded-3xl text-blue-50 w-[120px]"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+  return (
+    <div className="flex flex-1 flex-col h-full bg-gray-300"> 
       <Head>
         <title>Mixo Mate | Preferences</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main>
-        <br></br>
-        <br></br>
         <RenderCurrentStep />
       </main>
     </div>
-  )
+  );
 }
