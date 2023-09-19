d2b4e36c0eccedda8aab07fce7341d52a7b1d0e0
codyfp <fisherpeel.c@gmail.com>
2023-09-19 20:33:05 +1000

    feat: add visual indicator of the logged in status

--- apps/mixo-mate-frontend/components/Header.tsx
+++ apps/mixo-mate-frontend/components/Header.tsx
@@ -1,17 +1,37 @@
+import { useMemo } from 'react';
+import { UserApi } from './../clientApi/UserApi';
+import Link from 'next/link';
+
 const Header = ({ logoSrc }) => {
-    return (
-        <header className="bg-custom-orange shadow-md h-[14vh]">
-          <div className="container mx-auto flex items-center justify-between h-full"> 
-            
-            <img src={logoSrc} alt="Logo" className="h-full w-auto blend-multiply" />
-            <div className="cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-            </div>
-          </div>
-        </header>
-    )
+  const userApi = new UserApi();
+  const username: string | undefined = useMemo(async () => {
+    if (userApi) {
+      let name = await userApi.getCurrent()?.username;
+      return name;
+    }
+    return undefined;
+  }, [userApi]);
+
+  return (
+    <header className="bg-custom-orange shadow-md h-[14vh]">
+      <div className="container mx-auto flex items-center justify-between h-full">
+        <img src={logoSrc} alt="Logo" className="h-full w-auto blend-multiply" />
+        {username ? (
+          <div className="text-white mr-4">Welcome {username}</div>
+        ) : (
+          <Link href="/">
+            <a className="text-white mr-4">Login or Register</a>
+          </Link>
+        )}
+
+        <div className="cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-white">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
