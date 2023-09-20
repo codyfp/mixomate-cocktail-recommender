f229b9405fd4ee1557a6d86f259e7d25d7d54eeb
pharapeti <patrice.harapeti@gmail.com>
2023-09-20 16:55:15 +1000

    Flesh out backend Recommendation API

--- apps/mixo-mate-backend/src/recommendations/recommendation.generation.ts
+++ apps/mixo-mate-backend/src/recommendations/recommendation.generation.ts
@@ -1,34 +1,24 @@
 import { Cocktail } from '../cocktails/cocktail.dto.js';
-import { CocktailService } from '../cocktails/cocktail.service.js';
-import { RecommendationModel } from './recommendation.model.js';
 import { Recommendation } from './recommendation.js';
+import { RecommendationAPI } from './recommendation.api.js';
 
 export default class RecommendationGenerator {
-  constructor() {
-  }
-
   async generateNewRecommendations(userId: string, count: number = 5): Promise<Recommendation[]> {
-    // TODO: Get recommended cocktails for user from Recommendation ML model
+    const api = new RecommendationAPI();
+    const recommendedCocktails: Cocktail[] = await api.getRecommended(userId, count);
 
-    const cocktails: Cocktail[] = await new CocktailService().getAll();
-    const recommendedCocktails: Cocktail[] = cocktails.slice(0, count);
-
-    const recommendations: Recommendation[] = await Promise.all(
-      recommendedCocktails.map(async (cocktail: Cocktail) => {
-        const doc = await RecommendationModel.create({
-          userId: userId,
-          cocktailId: cocktail.id
-        })
-
-        return this._recordToRecommendation(doc)
-      })
-    )
-
-    return recommendations
+    return recommendedCocktails.map((cocktail: Cocktail): Recommendation => {
+      return { 
+        userId: userId, 
+        cocktailId: cocktail.id
+      }
+    })
   }
 
   async reviewRecommendation(review: Required<Recommendation>): Promise<void> {
-    // Use cocktail review to adjust ML model
+    const api = new RecommendationAPI();
+    await api.submitReview(review);
+
     return;
   }
 
