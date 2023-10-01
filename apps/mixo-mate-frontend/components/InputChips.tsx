8c929051d538d95ef20f2c500511f90e506abb30
aryangupta1 <aryangupta1@outlook.com>
2023-10-01 17:38:58 +1100

    Cleanup

--- apps/mixo-mate-frontend/components/InputChips.tsx
+++ apps/mixo-mate-frontend/components/InputChips.tsx
@@ -1,50 +1,48 @@
-import React, { useState } from 'react';
+import { useState } from 'react'
+import { InputLabel } from '@/types/InputLabels'
 
 interface InputChipProps {
-  title: string
+  title: InputLabel
   options: string[]
   setOptions: (options: string[]) => void
 }
 
 export default function InputChips({ title, options, setOptions }: InputChipProps) {
   const [input, setInput] = useState<string>("")
-  const [showDropdown, setShowDropdown] = useState<boolean>(false)
 
   const onChange = (text: string) => {
-    setInput(text);
-    setShowDropdown(text.length > 0);
+    setInput(text)
   }
 
-  const onAddItem = () => {
-    if (input.trim() !== "" && !options.includes(input.trim())) {
-      setOptions(prev => [...prev, input.trim()])
+  const onAddItem = (key: string) => {
+    if (key === "Enter" && input.trim() !== "") {
+      const _chips = [...options]
+      _chips.push(input)
+      setOptions(_chips)
       setInput("")
-      setShowDropdown(false)
     }
   }
 
-  const onSelectItem = (item: string) => {
-    setInput(item)
-    onAddItem()
-  }
-
   const onDeleteItem = (index: number) => {
-    setOptions(prev => prev.filter((_, i) => i !== index))
+    const _chips = [...options]
+    _chips.splice(index, 1)
+    setOptions(_chips)
   }
 
   return (
     <div className="bg-white px-5 py-4 min-h-[12rem] rounded-xl flex flex-col justify-between items-center">
-      <div className="relative">
+      <div>
         <div className="flex flex-row items-center mb-4">
-          <label className={`x-title w-32`}>{title}:</label>
+          <label className={`x-title w-32 ${title === 'Allergens' ? 'mr-4' : ''}`}>{title}:</label>
           <input 
             placeholder="Enter here"
             className="bg-gray-300 rounded-full h-8 w-60 px-5 uppercase"
             value={input}
             onChange={(e) => onChange(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && onAddItem()}
+            onKeyDown={(e) => onAddItem(e.key)}
           />
         </div>
+
         <div className="flex flex-row flex-wrap gap-1 mb-4">
           {options.map((chip, index) => (
             <Chip key={index} onClick={() => onDeleteItem(index)}>
@@ -57,15 +55,28 @@ export default function InputChips({ title, options, setOptions }: InputChipProp
   )
 }
 
-function Chip({ children, onClick }: { children: React.ReactNode; onClick?: () => void }) {
+function Chip({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode
+  onClick?: () => void
+}) {
   return (
     <div className="bg-gray-300 flex items-center gap-3 rounded-full pl-5 pr-2 h-8 w-max">
       <div className="font-bold uppercase">{children}</div>
-      <button className="hover:bg-gray-400 flex items-center justify-center rounded-full w-[20px] h-[20px]" onClick={onClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+      <button
+        className="hover:bg-gray-400 flex items-center justify-center rounded-full w-[20px] h-[20px]"
+        onClick={onClick}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 384 512"
+        >
           <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
         </svg>
       </button>
     </div>
-  )
+  );
 }
