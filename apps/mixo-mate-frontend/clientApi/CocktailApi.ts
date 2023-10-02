4dfac13cc3dee6faf760a529bf10d5baedccfddb
jdroman1912 <jdroman99@hotmail.com>
2023-10-03 10:38:41 +1100

    Cocktail card component and cocktail images
    Created the cocktail card and added images for testing

--- apps/mixo-mate-frontend/clientApi/CocktailApi.ts
+++ apps/mixo-mate-frontend/clientApi/CocktailApi.ts
@@ -2,15 +2,17 @@ import { Ingredient } from "./IngredientApi";
 import { MixoMateApi } from "./MixoMateApi";
 
 export type Cocktail = {
-  id: string,
-  name: string,
-  rating: number,
+  id: string;
+  name: string;
+  rating: number;
 
-  n_steps: number,
-  n_ingredients: number,
+  n_steps: number;
+  n_ingredients: number;
 
-  steps: string,
+  steps: string;
   ingredients: Ingredient[];
+
+  image_url: String;
 }
 
 export class CocktailApi extends MixoMateApi {
