30d8f4d32b3d2569b67c12d490e402e523a101d6
codyfp <fisherpeel.c@gmail.com>
2023-10-02 20:31:46 +1100

    feat: add a hook to get the current user acrosss components

---
+++ apps/mixo-mate-frontend/clientApi/hooks/useAuth.ts
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { UserApi } from '../UserApi';
+
+export const useAuth = () => {
+  const [userApi, setApi] = useState<UserApi | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState<unknown | null>(null);
+
+  useEffect(() => {
+    const api = new UserApi();
+    setApi(api);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (userApi) {
+      userApi.getCurrent().then((user) => {
+        setCurrentUser(user);
+      });
+    }
+  }, [userApi]);
+
+  const getCurrentUser = () => {
+    if (userApi) {
+      userApi.getCurrent().then((user) => {
+        setCurrentUser(user);
+      });
+    }
+  }
+
+  const authApi = {
+    login: async (username: string, password: string) => {
+      if (userApi) {
+        const response = await userApi.login(username, password);
+        getCurrentUser();
+        return response;
+      }
+    },
+    logout: async () => {
+      if (userApi) {
+        const response = await userApi.get('/logout');
+        getCurrentUser();
+        return response;
+      }
+    },
+    create: async (username: string, password: string) => {
+      if (userApi) {
+        const response = await userApi.create(username, password);
+        getCurrentUser();
+        return response;
+      }
+    }
+  }
+
+  return { userApi: authApi, loading, currentUser, getCurrentUser };
+};
