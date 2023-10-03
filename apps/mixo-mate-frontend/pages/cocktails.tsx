ea9664bfdd55b84de45f7832991f1a2eba161ad7
pharapeti <patrice.harapeti@gmail.com>
2023-10-03 22:48:38 +1100

    Render Cocktail images from the S3 bucket

--- apps/mixo-mate-frontend/pages/cocktails.tsx
+++ apps/mixo-mate-frontend/pages/cocktails.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import { Cocktail, CocktailApi } from "@/clientApi/CocktailApi";
+import Image from "next/image";
 
 export default function Cocktails() {
   const [cocktails, setCocktails] = useState<Cocktail[]>([]);
@@ -19,6 +20,10 @@ export default function Cocktails() {
 
     fetchCocktails();
   }, [])
+
+  const generateImageURL = (recipe_id: string) => {
+    return `https://mixomate-cocktails.s3.ap-southeast-2.amazonaws.com/${recipe_id}.jpg`
+  }
   
   return (
     <div>
@@ -33,18 +38,32 @@ export default function Cocktails() {
             <thead>
               <tr>
                 <th>Name</th>
+                <th>Recipe ID</th>
                 <th>Rating</th>
                 <th>Number of Steps</th>
                 <th>Number of Ingredients</th>
+                <th>Flavour Profile</th>
+                <th>Picture</th>
               </tr>
             </thead>
             <tbody>
               {cocktails.map((cocktail) => (
                 <tr key={cocktail.id} className="text-center">
                   <td>{cocktail.name}</td>
+                  <td>{cocktail.recipe_id}</td>
                   <td>{cocktail.rating}</td>
                   <td>{cocktail.n_steps}</td>
                   <td>{cocktail.n_ingredients}</td>
+                  <td>{cocktail.flavour_profile}</td>
+                  <td>
+                    <Image 
+                      src={generateImageURL(cocktail.recipe_id)}
+                      height={300}
+                      width={300}
+                      loading="lazy"
+                      alt={`Image of recipe ${cocktail.recipe_id}`}
+                    />
+                  </td>
                 </tr>
               ))}
             </tbody>
