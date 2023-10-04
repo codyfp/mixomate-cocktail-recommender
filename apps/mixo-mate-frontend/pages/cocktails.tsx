ae7f850c2765a534028fa13cf81b2f2c05c0a95b
aryangupta1 <aryangupta1@outlook.com>
2023-10-04 16:02:16 +1100

    add pagination, sorting and filtering

--- apps/mixo-mate-frontend/pages/cocktails.tsx
+++ apps/mixo-mate-frontend/pages/cocktails.tsx
@@ -7,6 +7,10 @@ import 'primeicons/primeicons.css';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
+const gridLineStyle = {
+  border: "1px solid #d1d1d1"
+};
+
 export default function Cocktails() {
   const [cocktails, setCocktails] = useState<Cocktail[]>([]);
 
@@ -34,11 +38,18 @@ export default function Cocktails() {
       <main>
         <div className='flex flex-col'>
           <h1>Cocktails</h1>
-          <DataTable value={cocktails}>
-            <Column field="name" header="Name"></Column>
-            <Column field="rating" header="Rating"></Column>
-            <Column field="n_steps" header="Number of Steps"></Column>
-            <Column field="n_ingredients" header="Number of Ingredients"></Column>
+          <DataTable 
+            value={cocktails} 
+            paginator 
+            rows={5} 
+            rowsPerPageOptions={[5, 10, 25, 50]}
+            sortMode="single" 
+            className="p-datatable-gridlines" 
+            rowStyle={gridLineStyle}>
+            <Column field="name" header="Name" sortable filter style={gridLineStyle}></Column>
+            <Column field="rating" header="Rating" sortable filter style={gridLineStyle}></Column>
+            <Column field="n_steps" header="Number of Steps" sortable filter style={gridLineStyle}></Column>
+            <Column field="n_ingredients" header="Number of Ingredients" sortable filter style={gridLineStyle}></Column>
           </DataTable>
         </div>
       </main>
