f808d74b7aa52cd6217c322a994d423b78f758ae
smolloy1 <13610225@student.uts.edu.au>
2023-10-08 15:34:16 +1100

    Integrated Model
    Converted datasets to pickle to save from using git lfs. Loading function from a python file to improve readability.  Reccomender function works inside the service.py

--- apps/mixo-mate-recommendations/app/api/recommendations/model.py
+++ apps/mixo-mate-recommendations/app/api/recommendations/model.py
@@ -1,31 +1,33 @@
-# Create Tensorflow model object
-# Load model.json generated from modelGenerator into Tensflow model object
+def get_similar_cocktails(input_value,combined_df,similarity_matrix, N=5):
+    """
+    Fetch similar cocktails based on a given cocktail name or ID.
+    
+    Args:
+    - input_value (str or int): Name or ID of the cocktail.
+    - N (int): Number of similar cocktails to return. Default is 5.
 
-from numpy import loadtxt
-from keras import models
-from tensorflow.keras.models import load_model
+    Returns:
+    - list: Names of top N similar cocktails.
+    """
+    
+    # Determine if input is name or ID
+    if isinstance(input_value, str):
+        if input_value not in combined_df['name'].values:
+            raise ValueError(f"No cocktail named {input_value} found in the dataset.")
+        cocktail_index = combined_df[combined_df['name'] == input_value].index[0]
+    elif isinstance(input_value, int):  # Assuming ID is an integer
+        if input_value not in combined_df['recipe_id'].values:
+            raise ValueError(f"No cocktail with ID {input_value} found in the dataset.")
+        cocktail_index = combined_df[combined_df['recipe_id'] == input_value].index[0]
+    else:
+        raise ValueError("Input value must be either a name (string) or an ID (integer).")
+    
+    # Fetch and enumerate similarity scores for the given cocktail
+    similar_scores = list(enumerate(similarity_matrix[cocktail_index]))
+    
+    # Sort the scores
+    sorted_similar_scores = sorted(similar_scores, key=lambda x: x[1], reverse=True)
+    
+    # Return the top N cocktail names excluding the input cocktail itself
+    return [combined_df.iloc[i[0]]['name'] for i in sorted_similar_scores[1:N+1]]
 
-class RecommendationModel:
-
-  def __init__(self):
-    try:
-      # self.tfModel = load_model('recommendationModel.h5')     # Load model
-      self.tfModel = models.Sequential()                        # Create temporary sequential model
-      # self.tfModel.summary()                                    # Summarise model
-
-      # self.dataset = loadtxt(                                 # Load dataset
-      #   "cocktails.csv",
-      #   delimiter=",")
-    except ValueError as e:
-      print("ERROR: Failed to load Recommendations model")
-      raise e
-
-  def test_model(self):
-    self.tfModel.summary()
-
-  def generate_recommendation_for_user(user_id, count=5):
-    print(f"Generating {count} recommendations for user with ID: {user_id}")
-    # Interact with self.tfModel to recommended {count} of cocktails for the given user
-    return []
-
-recommendationModel = RecommendationModel()
