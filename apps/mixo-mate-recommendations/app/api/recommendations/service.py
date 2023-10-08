a01ac613f0e5f123f49ae442a936feb5a4863528
smolloy1 <59944029+smolloy1@users.noreply.github.com>
2023-10-08 18:16:00 +1100

    Copied necessary files to docker so api/reccomendations works to retrieve reccomendations when http://localhost:5000/api/recommendations is called.

--- apps/mixo-mate-recommendations/app/api/recommendations/service.py
+++ apps/mixo-mate-recommendations/app/api/recommendations/service.py
@@ -5,9 +5,9 @@ import pandas as pd
 import os
 from model import get_similar_cocktails 
 
-file_path1 = os.path.join('apps', 'mixo-mate-recommendations', 'app', 'api', 'recommendations', 'compressed_combined_df.pkl.gz')
-file_path2 = os.path.join('apps', 'mixo-mate-recommendations', 'app', 'api', 'recommendations', 'compressed_similarity_matrix.h5')
-
+# Adjusted paths for Docker container
+file_path1 = os.path.join('/app', 'compressed_combined_df.pkl.gz')
+file_path2 = os.path.join('/app', 'compressed_similarity_matrix.h5')
 
 with gzip.open(file_path1, 'rb') as f:
     combined_df = pickle.load(f)
@@ -15,19 +15,12 @@ with gzip.open(file_path1, 'rb') as f:
 with h5py.File(file_path2, "r") as hf:
     similarity_matrix = hf["similarity_matrix"][:]
 
-
-
-
-print(similarity_matrix.shape)
-
 class RecommendationService:
     @staticmethod
     def generate_for_user(user_id):
         try:
             # Use RecommendationModel
-            return get_similar_cocktails(182985,combined_df,similarity_matrix,5)
+            return get_similar_cocktails(182985,combined_df=combined_df,similarity_matrix=similarity_matrix,N=5)
 
         except Exception as error:
             raise Exception(f'Failed to generate recommendations. {error}')
-
-print(get_similar_cocktails(182985,combined_df=combined_df,similarity_matrix=similarity_matrix,N=5))
