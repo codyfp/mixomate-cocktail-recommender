e1bd11ee14f82d6efc9f7c97a251057e286f1dfe
smolloy1 <13610225@student.uts.edu.au>
2023-10-08 15:34:16 +1100

    Integrated Model
    Converted datasets to pickle to save from using git lfs. Loading function from a python file to improve readability.  Reccomender function works inside the service.py

--- apps/mixo-mate-recommendations/app/api/recommendations/service.py
+++ apps/mixo-mate-recommendations/app/api/recommendations/service.py
@@ -1,11 +1,28 @@
-from .model import recommendationModel
+import pickle
+import gzip
+import h5py
+import pandas as pd
+from model import get_similar_cocktails 
+
+with gzip.open(r'apps\mixo-mate-recommendations\app\api\recommendations\compressed_combined_df.pkl.gz', 'rb') as f:
+    combined_df = pickle.load(f)
+
+with h5py.File(r"apps\mixo-mate-recommendations\app\api\recommendations\compressed_similarity_matrix.h5", "r") as hf:
+    similarity_matrix = hf["similarity_matrix"][:]
+
+
+
+
+print(similarity_matrix.shape)
 
 class RecommendationService:
     @staticmethod
     def generate_for_user(user_id):
         try:
             # Use RecommendationModel
-            return recommendationModel.generate_recommendation_for_user(user_id)
+            return get_similar_cocktails(182985,combined_df,similarity_matrix,5)
 
         except Exception as error:
             raise Exception(f'Failed to generate recommendations. {error}')
+
+print(get_similar_cocktails(182985,combined_df=combined_df,similarity_matrix=similarity_matrix,N=5))
