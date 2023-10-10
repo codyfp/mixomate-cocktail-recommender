ec1e696a3c83bbcd10a5e1f556163afeb640502d
jdroman1912 <jdroman99@hotmail.com>
2023-10-11 01:01:14 +1100

    Deleting changes in header
    When solving the conflicts we ended up having the same code twice so we get rid of the extra part.

--- apps/mixo-mate-frontend/components/Header.tsx
+++ apps/mixo-mate-frontend/components/Header.tsx
@@ -44,25 +44,5 @@ const Header = () => {
   )
 }
 
-        <div className="cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="h-6 w-6 text-white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </div>
-      </div>
-    </header>
-  );
-};
 
 export default Header;
