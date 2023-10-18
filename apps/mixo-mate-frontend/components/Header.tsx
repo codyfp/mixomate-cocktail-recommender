b9ce9474257f8774e1eabfbab6209e1d049fc62d
Patrice Harapeti <patrice.harapeti@gmail.com>
2023-10-18 17:59:39 +1100

    Use PrimeReact menu component to improve the navbar (#51)

--- apps/mixo-mate-frontend/components/Header.tsx
+++ apps/mixo-mate-frontend/components/Header.tsx
@@ -1,9 +1,20 @@
-import { useMemo } from 'react';
-import { UserApi } from './../clientApi/UserApi';
+// Next/React
+import { useMemo, useRef } from 'react';
 import Image from 'next/image';
+
+// PrimeReact
+import { Menu } from 'primereact/menu';
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+// API
+import { UserApi } from './../clientApi/UserApi';
 
 const Header = () => {
   const userApi = new UserApi();
+
   const username = useMemo(() => {
     if (userApi) {
       let name = userApi.getCurrent();
@@ -12,6 +23,16 @@ const Header = () => {
     return undefined;
   }, [userApi]);
 
+  const menu = useRef(null);
+
+  const items = [
+    {label: 'Home', icon: 'pi pi-fw pi-home', command: () => { window.location.href = "/"; }},
+    {label: 'Preferences', icon: 'pi pi-fw pi-cog', command: () => { window.location.href = "/preferences"; }},
+    {label: 'Recommendations', icon: 'pi pi-fw pi-shopping-bag', command: () => { window.location.href = "/recommendations"; }},
+    {label: 'Cocktails', icon: 'pi pi-fw pi-database', command: () => { window.location.href = "/cocktails"; }},
+    {label: 'Ingredients', icon: 'pi pi-fw pi-database', command: () => { window.location.href = "/ingredients"; }}
+  ];
+
   return (
     <header className="bg-custom-orange shadow-md h-[14vh]">
       <div className="container mx-auto flex items-center justify-between h-full">
@@ -19,7 +40,7 @@ const Header = () => {
           src="/images/logo.png" 
           alt="Logo" 
           priority={true}
-          className="h-full w-auto blend-multiply"
+          className="h-full w-auto blend-multiply cursor-pointer"
           height={200}
           width={200}
           onClick={() => window.location.replace('/')}
@@ -34,15 +55,19 @@ const Header = () => {
           </Link>
         )} */}
 
-        <div className="cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-white">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-          </svg>
+        <div className="mt-8">
+          <Menu model={items} popup ref={menu} id="popup_menu" />
+          <Button 
+            className="text-white text-lg"
+            icon="pi pi-bars"
+            onClick={(event) => menu.current.toggle(event)}
+            aria-controls="popup_menu"
+            aria-haspopup
+          />
         </div>
       </div>
     </header>
   )
 }
-
 
 export default Header;
