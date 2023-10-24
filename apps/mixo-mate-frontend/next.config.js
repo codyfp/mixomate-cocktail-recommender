a3f74eb1e60eadbb8edb622724bc5d9d299ef6a8
codyfp <fisherpeel.c@gmail.com>
2023-10-24 19:23:06 +1100

    chore: redirect /auth to authenticate route

--- apps/mixo-mate-frontend/next.config.js
+++ apps/mixo-mate-frontend/next.config.js
@@ -1,14 +1,27 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
 const nextConfig = {
   reactStrictMode: false,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'mixomate-cocktails.s3.ap-southeast-2.amazonaws.com',
+        protocol: "https",
+        hostname: "mixomate-cocktails.s3.ap-southeast-2.amazonaws.com",
       },
     ],
   },
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/auth",
+        destination: "/authenticate",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
