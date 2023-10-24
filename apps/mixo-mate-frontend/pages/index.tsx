8875ff18cc31be913e81b57e6b0549577874fa0b
jdroman1912 <jdroman99@hotmail.com>
2023-10-24 22:32:26 +1100

    Redirect function

--- apps/mixo-mate-frontend/pages/index.tsx
+++ apps/mixo-mate-frontend/pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head';
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   const list = [
     "Pick your preferences",
     "Build your flavour profile",
     "Enjoy hundreds of recommendations!",
     "No experience required",
   ];
+
+  const goToAuth = () => router.push("/authenticate");
 
   return (
     <>
@@ -15,11 +19,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="bg-[url('/images/index-background.jpg')] bg-no-repeat bg-cover bg-[left_-80px] text-white py-28 px-16 flex h-full w-full">
+      <div className="bg-[url('/images/index-background.jpg')] bg-no-repeat bg-cover bg-[left_-80px] text-white py-20 px-16 flex flex-1 h-full w-full">
         <div className="flex flex-1 flex-col justify-between h-full w-full">
           <h1 className="x-title !text-7xl">MIXOMATE</h1>
 
-          <button className="x-title bg-red-900 w-max py-5 px-12 rounded-3xl hover:bg-red-950">
+          <button
+            className="x-title bg-red-900 w-max py-5 px-12 rounded-3xl hover:bg-red-950"
+            onClick={goToAuth}
+          >
             Get Started!
           </button>
         </div>
