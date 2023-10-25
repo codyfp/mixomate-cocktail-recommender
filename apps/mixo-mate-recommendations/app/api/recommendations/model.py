f4967b27ac075a4bd2c1463a94260481315d88fc
pharapeti <patrice.harapeti@gmail.com>
2023-10-25 17:25:15 +1100

    Fix Recommendation model weightings - Ensure model can handle ingredients that do not appear in the matrix

--- apps/mixo-mate-recommendations/app/api/recommendations/model.py
+++ apps/mixo-mate-recommendations/app/api/recommendations/model.py
@@ -10,25 +10,24 @@ class RecommendationModel:
         with gzip.open(DF_FILE_PATH, 'rb') as f:
             self.combined_df = pickle.load(f)
 
-    def adjust_score_for_cocktail(self, cocktail, score, allergen_ids, like_ids, dislike_ids,flavour_profile):
+    def adjust_score_for_cocktail(self, cocktail, score, allergen_ids, like_ids, dislike_ids, flavour_profile):
         """Adjust the similarity score based on user preferences using ingredient IDs."""
+        for allergen in allergen_ids:
+            if allergen in cocktail and cocktail[allergen] == 1.0:
+                score = -1e9
+                return score
+
         for profile in flavour_profile:
             if profile in cocktail['flavour_profile']:
-                score =+ 0.5
-        
-        
-        for allergen in allergen_ids:
-            if cocktail[allergen] == 1.0:
-                score -= -10
-
+                score += 0.5
 
         for like in like_ids:
-            if cocktail[like] == 1.0:
+            if like in cocktail and cocktail[like] == 1.0:
                 score += 1
 
         for dislike in dislike_ids:
-            if cocktail[dislike] == 1.0:
-                score *= 0.80
+            if dislike in cocktail and cocktail[dislike] == 1.0:
+                score -= 0.50
 
         return score
 
@@ -38,7 +37,9 @@ class RecommendationModel:
         for idx, cocktail in self.combined_df.iterrows():
             user_scores[idx] = self.adjust_score_for_cocktail(cocktail, 1, allergen_ids, like_ids, dislike_ids,flavour_profile)
 
-        sorted_indices = np.argsort(user_scores)[::-1][:N]
+        # Only recommend cocktails that a score > 0
+        positive_user_stores = np.where(user_scores > 0)[0]
+        sorted_indices = np.argsort(positive_user_stores)[::-1][:N]
         return self.combined_df.iloc[sorted_indices]['recipe_id'].tolist()
 
 # Create and expose a single model
